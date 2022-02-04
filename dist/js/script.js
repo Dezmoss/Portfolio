1d500fb25014146347254b56e2c66c67d1edf6a7
Denis Kuznetsov <detrevile3@gmail.com>
2022-02-04 15:39:50 +0300

    Skills section complete

--- dist/js/script.js
+++ dist/js/script.js
@@ -9,3 +9,13 @@ hamburger.addEventListener('click', () => {
 closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
 });
+
+const percent = document.querySelectorAll('.skills__percent'),
+    progress = document.querySelectorAll('.skills__yellow-progress');
+
+console.log(percent);
+console.log(progress);
+
+percent.forEach((item, i) => {
+    progress[i].style.width = item.innerText;
+});
