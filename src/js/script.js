0e95a3dff5589c3c17c73ac474db549f7c7df42a
Denis Kuznetsov <detrevile3@gmail.com>
2022-02-07 13:56:32 +0300

    Adapt design fix

--- src/js/script.js
+++ src/js/script.js
@@ -1,23 +1,40 @@
-//Menu button
-const hamburger = document.querySelector('.hamburger'),
-    menu = document.querySelector('.menu'),
-    closeElem = document.querySelector('.menu__close');
+document.addEventListener('DOMContentLoaded', ()=> {
 
-hamburger.addEventListener('click', () => {
-    menu.classList.add('active');
-});
+    //Menu button
+    const hamburger = document.querySelector('.hamburger'),
+        menu = document.querySelector('.menu'),
+        closeElem = document.querySelector('.menu__close');
 
-closeElem.addEventListener('click', () => {
-    menu.classList.remove('active');
-});
+    hamburger.addEventListener('click', () => {
+        menu.classList.add('active');
+    });
 
-//Skill bars
-const percent = document.querySelectorAll('.skills__percent'),
-    progress = document.querySelectorAll('.skills__yellow-progress');
+    closeElem.addEventListener('click', () => {
+        menu.classList.remove('active');
+    });
 
-console.log(percent);
-console.log(progress);
+    const overlay = document.querySelector('.menu__overlay'),
+          menuLinks = document.querySelectorAll('.menu__list a');
 
-percent.forEach((item, i) => {
-    progress[i].style.width = item.innerText;
+    menuLinks.forEach(item => {
+        item.addEventListener('click', ()=> {
+            menu.classList.remove('active');
+        });
+    });
+          
+    overlay.addEventListener('click', (e) => {
+        if (e.target && e.target === overlay) {
+            menu.classList.remove('active');
+        }
+    });
+
+    //Skill bars
+    const percent = document.querySelectorAll('.skills__percent'),
+        progress = document.querySelectorAll('.skills__yellow-progress');
+
+    percent.forEach((item, i) => {
+        progress[i].style.width = item.innerText;
+    });
+
+
 });
